6ba12ed5077c325cf4d65ee4602609deccde1505
Caleb Byrd <calebnathanbyrd@gmail.com>
2025-11-26 18:07:00 -0500

    add new symlinks

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Abbreviations
+abbr -a cl clear
+abbr -a l ll -a
+abbr -a gco git checkout
+abbr -a gp git push
+abbr -a lg lazygit
+abbr -a n nvim
+abbr -a p python
+abbr -a c claude
+abbr -a es "cd ~/esim; source .venv/bin/activate.fish"
