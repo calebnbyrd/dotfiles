9fe773b538a0756c643a3c0f0954258e9f1ff7a8
Caleb Byrd <calebnathanbyrd@gmail.com>
2026-01-15 18:00:04 -0500

    update to new windowrule syntax

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,6 +12,7 @@ abbr -a n nvim
 abbr -a p python
 abbr -a c claude
 abbr -a es "cd ~/esim; source .venv/bin/activate.fish"
+abbr -a e1 "cd ~/esim-agent1; source ~/esim/.venv/bin/activate.fish"
 abbr -a e "python src/main.py &; sleep 0.5; hyprctl dispatch setfloating; hyprctl dispatch resizeactive exact 1920 1080; hyprctl dispatch centerwindow"
 
 # Initialize starship
@@ -22,12 +23,12 @@ zoxide init fish | source
 
 # Yazi shell wrapper (to use "y" and change current working directory when exiting)
 function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
 end
 
 # Environment variables
